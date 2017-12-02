6ebf6e292bbba41e63736e471228ba71b19cd516
Winston <winston.yongwei@gmail.com>
2017-12-02 17:59:20 +0800

    Refactoring and add todos.

--- main.go
+++ main.go
@@ -2,6 +2,13 @@
 // - https://github.com/apex/up-examples/blob/master/oss/golang-uploads/main.go
 // - https://github.com/choonkeat/attache/blob/master/lib/attache/upload.rb
 
+// todos:
+// 1. tests
+// 2. apex logging?
+// 3. validation? prevent image bomb?
+// 4. exif?
+// 5. documentation
+
 package main
 
 import (
@@ -27,36 +34,20 @@ import (
 // }
 
 func main() {
-  http.HandleFunc("/upload", upload)
+  http.HandleFunc("/", apiRequest)
 
+  log.Printf("Listening to %s...", os.Getenv("PORT"))
   if err := http.ListenAndServe(":" + os.Getenv("PORT"), nil); err != nil {
     log.Fatalf(fmt.Sprintf("Error Listening - %s", err))
   }
 }
 
-func upload(w http.ResponseWriter, r *http.Request) {
+func apiRequest(w http.ResponseWriter, r *http.Request) {
   log.Printf("%s", r)
 
   switch r.Method {
   case "POST", "PUT", "PATCH":
-
-    r.ParseMultipartForm(32 << 20)
-    file, handler, err := r.FormFile("file")
-    if err != nil {
-      // log.WithError(err).Error("Parsing Form")
-      http.Error(w, fmt.Sprintf("Error Parsing Form - %s", err), http.StatusBadRequest)
-      return
-    }
-    defer file.Close()
-
-    fmt.Fprintf(w, fmt.Sprintf("Uploaded: %s, of type %s", handler.Filename, handler.Header.Get("Content-Type")))
-
-    // bytes, err := ioutil.ReadAll(file)
-    // if err != nil {
-    //   // log.WithError(err).Error("Reading File")
-    //   http.Error(w, "Error Reading File.", http.StatusInternalServerError)
-    //   return
-    // }
+    readFile(w, r) // is this refactoring ok?
 
   case "OPTIONS":
     w.Header().Set("Access-Control-Allow-Methods", "POST, PUT, PATCH, OPTIONS")
@@ -64,4 +55,26 @@ func upload(w http.ResponseWriter, r *http.Request) {
   default:
     http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
   }
+}
+
+func readFile(w http.ResponseWriter, r *http.Request) {
+  r.ParseMultipartForm(32 << 20)
+  file, handler, err := r.FormFile("file")
+  if err != nil {
+    // log.WithError(err).Error("Parsing Form")
+    http.Error(w, fmt.Sprintf("Error Parsing Form - %s", err), http.StatusBadRequest)
+    return
+  }
+  defer file.Close()
+
+  fmt.Fprintf(w, fmt.Sprintf("Uploaded: %s, of type %s", handler.Filename, handler.Header.Get("Content-Type")))
+}
+
+func toS3() {
+  // bytes, err := ioutil.ReadAll(file)
+  // if err != nil {
+  //   // log.WithError(err).Error("Reading File")
+  //   http.Error(w, "Error Reading File.", http.StatusInternalServerError)
+  //   return
+  // }
 }
