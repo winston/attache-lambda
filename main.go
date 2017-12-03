f2545bea7fc4ce205e61ffd64832beee9de74132
Winston <winston.yongwei@gmail.com>
2017-12-04 00:30:26 +0800

    WIP refactoring.

--- main.go
+++ main.go
@@ -6,8 +6,9 @@
 // 1. tests
 // 2. apex logging?
 // 3. validation? prevent image bomb?
-// 4. exif?
-// 5. documentation
+// 4. protect API?
+// 5. exif?
+// 6. documentation
 
 package main
 
@@ -18,6 +19,10 @@ import (
   "log"
   "net/http"
   "os"
+
+  "github.com/aws/aws-sdk-go/aws"
+  "github.com/aws/aws-sdk-go/aws/session"
+  "github.com/aws/aws-sdk-go/service/s3/s3manager"
 
   // "github.com/apex/log"
   // "github.com/apex/log/handlers/json"
@@ -45,9 +50,13 @@ func main() {
 func apiRequest(w http.ResponseWriter, r *http.Request) {
   log.Printf("%s", r)
 
+  // Validation
+
   switch r.Method {
   case "POST", "PUT", "PATCH":
-    readFile(w, r) // is this refactoring ok?
+    file = readFile(w, r) // is this refactoring ok?
+
+    toS3(w, r, file)
 
   case "OPTIONS":
     w.Header().Set("Access-Control-Allow-Methods", "POST, PUT, PATCH, OPTIONS")
@@ -68,13 +77,56 @@ func readFile(w http.ResponseWriter, r *http.Request) {
   defer file.Close()
 
   fmt.Fprintf(w, fmt.Sprintf("Uploaded: %s, of type %s", handler.Filename, handler.Header.Get("Content-Type")))
+
+  return file
+
 }
 
-func toS3() {
-  // bytes, err := ioutil.ReadAll(file)
-  // if err != nil {
-  //   // log.WithError(err).Error("Reading File")
-  //   http.Error(w, "Error Reading File.", http.StatusInternalServerError)
-  //   return
-  // }
+func toS3(w http.ResponseWriter, r *http.Request, file File) {
+  bucket := r.FormValue("bucket")
+  log.Printf(bucket)
+
+  sess := session.NewSession(}
+  svc  := s3manager.NewUploader(sess)
+
+  _, err = uploader.Upload(&s3manager.UploadInput{
+      Bucket: aws.String(bucket),
+      Key: aws.String(filename),
+      Body: file,
+  })
+  if err != nil {
+      // Print the error and exit.
+      exitErrorf("Unable to upload %q to %q, %v", filename, bucket, err)
+  }
+
+  fmt.Printf("Successfully uploaded %q to %q\n", filename, bucket)
+
+
+  }
 }
+
+
+// func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+//   bucket := r.URL.Query().Get("bucket")
+//   s3key := r.URL.Query().Get("s3key")
+
+//   sess := session.New()
+//   svc := s3.New(sess)
+//   file, err := svc.GetObject(&s3.GetObjectInput{
+//     Bucket: &bucket,
+//     Key:    &s3key,
+//   })
+//   if err != nil {
+//     http.Error(w, err.Error(), http.StatusInternalServerError)
+//     return
+//   }
+//   defer file.Body.Close()
+
+//   // optional
+//   w.Header().Set("Content-Type", *file.ContentType)
+//   w.Header().Set("ETag", *file.ETag)
+
+//   // streaming
+//   io.Copy(w, file.Body)
+// }
+
