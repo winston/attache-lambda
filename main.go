53da80780cb3749041a985033ea21f256e9a1e4e
Winston <winston.yongwei@gmail.com>
2017-12-05 00:01:01 +0800

    Allow with s3 upload.

--- main.go
+++ main.go
@@ -13,20 +13,20 @@
 package main
 
 import (
-  // "encoding/base64"
-  // "io/ioutil"
-  "fmt"
-  "log"
-  "net/http"
-  "os"
+	// "encoding/base64"
+	// "io/ioutil"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+  "mime/multipart"
 
-  "github.com/aws/aws-sdk-go/aws"
-  "github.com/aws/aws-sdk-go/aws/session"
-  "github.com/aws/aws-sdk-go/service/s3/s3manager"
-
-  // "github.com/apex/log"
-  // "github.com/apex/log/handlers/json"
-  // "github.com/apex/log/handlers/text"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	// "github.com/apex/log"
+	// "github.com/apex/log/handlers/json"
+	// "github.com/apex/log/handlers/text"
 )
 
 // Use JSON logging when run by Up (including `up start`).
@@ -39,67 +39,65 @@ import (
 // }
 
 func main() {
-  http.HandleFunc("/", apiRequest)
+	http.HandleFunc("/", apiRequest)
 
-  log.Printf("Listening to %s...", os.Getenv("PORT"))
-  if err := http.ListenAndServe(":" + os.Getenv("PORT"), nil); err != nil {
-    log.Fatalf(fmt.Sprintf("Error Listening - %s", err))
-  }
+	log.Printf("Listening to %s...", os.Getenv("PORT"))
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatalf(fmt.Sprintf("Error Listening - %s", err))
+	}
 }
 
 func apiRequest(w http.ResponseWriter, r *http.Request) {
-  log.Printf("%s", r)
+	log.Printf("%s", r)
 
-  // Validation
+	// Validation
 
-  switch r.Method {
-  case "POST", "PUT", "PATCH":
-    file = readFile(w, r) // is this refactoring ok?
+	switch r.Method {
+	case "POST", "PUT", "PATCH":
+		file := readFile(w, r) // is this refactoring ok?
 
-    toS3(w, r, file)
+		toS3(w, r, file)
 
-  case "OPTIONS":
-    w.Header().Set("Access-Control-Allow-Methods", "POST, PUT, PATCH, OPTIONS")
+	case "OPTIONS":
+		w.Header().Set("Access-Control-Allow-Methods", "POST, PUT, PATCH, OPTIONS")
 
-  default:
-    http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-  }
+	default:
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	}
 }
 
-func readFile(w http.ResponseWriter, r *http.Request) {
-  r.ParseMultipartForm(32 << 20)
-  file, handler, err := r.FormFile("file")
-  if err != nil {
-    // log.WithError(err).Error("Parsing Form")
-    http.Error(w, fmt.Sprintf("Error Parsing Form - %s", err), http.StatusBadRequest)
-    return
-  }
-  defer file.Close()
+func readFile(w http.ResponseWriter, r *http.Request) multipart.File {
+	r.ParseMultipartForm(32 << 20)
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		// log.WithError(err).Error("Parsing Form")
+		http.Error(w, fmt.Sprintf("Error Parsing Form - %s", err), http.StatusBadRequest)
+		os.Exit(1)
+	}
+	defer file.Close()
 
-  // fmt.Fprintf(w, fmt.Sprintf("Uploaded: %s, of type %s", handler.Filename, handler.Header.Get("Content-Type")))
+	fmt.Fprintf(w, fmt.Sprintf("Uploaded: %s, of type %s", handler.Filename, handler.Header.Get("Content-Type")))
 
-  return file
+	return file
 }
 
-func toS3(w http.ResponseWriter, r *http.Request, file File) {
-  bucket := r.FormValue("bucket")
-  log.Printf(bucket)
+func toS3(w http.ResponseWriter, r *http.Request, file multipart.File) {
+	bucket := r.FormValue("bucket")
+	log.Printf(bucket)
 
-  sess := session.New()
-  svc  := s3manager.NewUploader(sess)
+	sess := session.New()
+	svc := s3manager.NewUploader(sess)
 
-  filename = file.Filename
+	filename := "abc"
 
-  _, err =  svc.Upload(
-              &s3manager.UploadInput{
-                Bucket: aws.String(bucket),
-                Key: aws.String(filename),
-                Body: file,
-              }
-            )
-  if err != nil {
-      log.Fatalf("Unable to Upload %s to %s, %s", filename, bucket, err)
-  }
+	_, err := svc.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(filename),
+		Body:   file})
 
-  fmt.Fprintf(w, "Successfully Uploaded %s to %s", filename, bucket)
+	if err != nil {
+		log.Fatalf("Unable to Upload %s to %s, %s", filename, bucket, err)
+	}
+
+	fmt.Fprintf(w, "Successfully Uploaded %s to %s", filename, bucket)
 }
